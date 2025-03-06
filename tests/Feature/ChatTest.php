ef5a70c1d559f1a6d6648457e21140772d134f2f
Gavin Abeele <abeele.s.gavin@gmail.com>
2025-03-06 09:24:36 -0500

    fixing failing tests

--- tests/Feature/ChatTest.php
+++ tests/Feature/ChatTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Chat;
+use App\Models\Message;
 use App\Models\User;
 use Inertia\Testing\AssertableInertia;
 use Tests\TestCase;
@@ -50,22 +51,41 @@ class ChatTest extends TestCase
 
         $chat = Chat::factory()->create([
             'user_id' => $userA->id,
+            'name' => 'Test Chat'
         ]);
 
         $chat->users()->attach($userA);
         $chat->users()->attach($userB);
 
-        Message::factory()->create([
+        $message = Message::factory()->create([
             'chat_id' => $chat->id,
             'user_id' => $userA->id,
+            'message' => 'Hello World'
         ]);
 
         $response = $this->actingAs($userA)->get(route('chat.show', $chat));
 
         $response->assertInertia(fn (AssertableInertia $page) => $page
-            ->component('Chat')
-            ->has('message.store', fn (AssertableInertia $page) => $page
-            ));
-
+            ->component('Chat/Show')
+            ->has('chat.data', fn (AssertableInertia $page) => $page
+                ->where('id', $chat->id)
+                ->where('name', 'Test Chat')
+                ->where('owner.id', $userA->id)
+                ->where('owner.name', $userA->name)
+                ->where('owner.email', $userA->email)
+                ->has('members.0', 2)
+                ->has('created_at')
+                ->has('updated_at')
+            )
+            ->has('messages.data', 1, fn (AssertableInertia $page) => $page
+                ->where('id', $message->id)
+                ->where('message', 'Hello World')
+                ->where('sender.user_id', $userA->id)
+                ->where('sender.name', $userA->name)
+                ->has('created_at')
+                ->has('updated_at')
+            )
+            ->where('auth.user.id', $userA->id)
+        );
     }
 }
