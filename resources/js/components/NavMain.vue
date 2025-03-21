29df24485cd389ddc30107366a428da6eca657fe
Gavin Abeele <abeele.s.gavin@gmail.com>
2025-03-21 09:22:35 -0400

    added badge to the sidebar

--- resources/js/components/NavMain.vue
+++ resources/js/components/NavMain.vue
@@ -2,6 +2,7 @@
 import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { ChatNavItem, type SharedData } from '@/types';
 import { Link, usePage } from '@inertiajs/vue3';
+import Badge from '@/components/ui/badge/Badge.vue';
 
 defineProps<{
     items: ChatNavItem[];
@@ -11,19 +12,15 @@ const page = usePage<SharedData>();
 </script>
 
 <template>
-    <SidebarGroup class="px-2 py-0">
+    <SidebarGroup class="px-2 py-2">
         <SidebarGroupLabel>Chats</SidebarGroupLabel>
         <SidebarMenu>
             <SidebarMenuItem v-for="item in items" :key="item.name">
                 <SidebarMenuButton as-child :is-active="item.href === page.url">
                     <Link :href="item.href" prefetch cache-for="10s" class="relative">
                         <component :is="item.icon" />
-                        <span>{{ item.name }}</span>
-                        <span 
-                            v-if="item.hasNewMessage"
-                            class="absolute right-0 top-0 -translate-y-1/2 translate-x-1/2 h-2 w-2 rounded-full bg-primary"
-                            aria-hidden="true"
-                        />
+                        <span class="truncate block max-w-[150px] overflow-hidden text-ellipsis whitespace-nowrap">{{ item.name }}</span>
+                        <Badge v-if="item.hasNewMessage" variant="highlight" class="text-sm my-1">Unread</Badge>
                     </Link>
                 </SidebarMenuButton>
             </SidebarMenuItem>
